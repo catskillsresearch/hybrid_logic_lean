596f8cca98df7903c5cb3fe8222ce56b67f90ec7
Lars Ericson <lars.ericson@catskillsresearch.com>
2024-12-11 10:51:11 -0500

    toolchain

--- lakefile.lean
+++ lakefile.lean
@@ -1,16 +1,28 @@
 import Lake
 open Lake DSL
 
-package hybrid {
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git"
+
+package Hybrid {
   -- add package configuration options here
 }
 
+@[default_target]
 lean_lib Hybrid {
-  -- add library configuration options here
+  -- Explicitly list all your .lean files here
+  roots := #[
+    `Hybrid.PROP, `Hybrid.TotalSet, `Hybrid.SVAR, `Hybrid.TypeIff,
+    `Hybrid.Completeness, `Hybrid.Eval, `Hybrid.Model, `Hybrid.Tautology,
+    `Hybrid.Substitutions, `Hybrid.IteratedModalities, `Hybrid.Variables,
+    `Hybrid.New_NOM, `Hybrid.NominalSubstitution, `Hybrid.FormSubstitution,
+    `Hybrid.Nominals, `Hybrid.NOM, `Hybrid.Form, `Hybrid.Util, `Hybrid.Proof,
+    `Hybrid.Tautologies, `Hybrid.Variants, `Hybrid.Satisfaction, `Hybrid.Truth,
+    `Hybrid.Lemmas, `Hybrid.Soundness, `Hybrid.ExistenceLemma, `Hybrid.ListUtils,
+    `Hybrid.RenameBound, `Hybrid.GeneralModel, `Hybrid.Canonical,
+    `Hybrid.GeneratedSubmodel, `Hybrid.WitnessedModel, `Hybrid.ProofUtils,
+    `Hybrid.FormCountable, `Hybrid.MCS, `Hybrid.CompletedModel,
+    `Hybrid.StandardCompletedModel, `Hybrid.Lindenbaum, `Hybrid.LanguageExtension
+  ]
 }
 
-@[default_target]
-lean_lib hybrid
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
